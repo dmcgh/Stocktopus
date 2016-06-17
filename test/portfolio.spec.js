bad666e3b8fb11d6bf425917bc396ba537458c25
Daryl McGhee <mcgheedaryl0331@gmail.com>
2016-06-17 12:06:21 -0500

    Adding portfolio and portfolio tests

---
+++ test/portfolio.spec.js
@@ -0,0 +1,48 @@
+// add protfolio
+// add stock
+// add stock
+// check length
+// check value
+// sell stocks
+// check value again
+
+/* eslint-disable func-names, prefer-arrow-callback*/
+
+const expect = require('chai').expect;
+const Portfolio = require('../lib/portfolio');
+const Stock = require('../lib/stock');
+// const nock = require('nock');
+
+describe('Portfolio', () => {
+  describe('constructor', function () {
+    it('should construct a new Portfolio object', function () {
+      const p1 = new Portfolio('Port_One');
+      expect(p1).to.be.instanceof(Portfolio);
+      expect(p1.name).to.equal('Port_One');
+    });
+  });
+  describe('#addStock', function () {
+    it('should add a new stock to Portfolio', function () {
+      const p1 = new Portfolio('Port_One');
+      const s1 = new Stock('Stock_One');
+      p1.addStock(s1);
+      expect(p1.stocks).to.be.length(1);
+      expect(p1.stocks[0]).to.be.instanceof(Stock);
+    });
+  });
+  describe('#position', function () {
+    it('should add the value of all stock in the portfolio', function () {
+      const p1 = new Portfolio('Port_One');
+      const s1 = new Stock('Stock_One');
+      const s2 = new Stock('Stock_Two');
+      p1.addStock(s1);
+      s1.shares = 2;
+      s1.purchasePricePerShare = 10;
+      p1.addStock(s2);
+      s2.shares = 3;
+      s2.purchasePricePerShare = 10;
+      expect(p1.stocks).to.be.length(2);
+      expect(p1.position()).to.equal(50);
+    });
+  });
+});
