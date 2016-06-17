b15743fe3cf6960665699d647127111fcd532aba
Daryl McGhee <mcgheedaryl0331@gmail.com>
2016-06-17 10:53:49 -0500

    fixing sell, purchase and adding constructor

--- test/stock.spec.js
+++ test/stock.spec.js
@@ -1,37 +1,67 @@
-/*eslint-disable func-names, prefer-arrow-callback*/
+/* eslint-disable func-names, prefer-arrow-callback*/
+
 const expect = require('chai').expect;
 const Stock = require('../lib/stock');
-describe('Stock', function( ) {
-  describe('constructor', function( ) {
-    it('should give you stock prices', function(done) {
-      const s1 = new Stock('ALL');
-      s1.purchase(50, (err, totalprice) => {
-              expect(s1).to.be.instanceof(Stock);
-              expect(s1.shares).to.equal(50);
-              expect(s1.purchasePricePerShare).to.equal(66.475);
-              expect(s1.name).to.equal('Allstate Corp');
-            //expect(s1.totalprice).to.be.above(3300);
-              done();
-            });
+const nock = require('nock');
+const sinon = require('sinon');
+let clock;
+
+describe('Stock', () => {
+  beforeEach(() => {
+    clock = sinon.useFakeTimers();
+    nock('http://dev.markitondemand.com')
+    .get('/MODApis/Api/v2/Quote/json?symbol=ALL')
+    .reply(200, {
+      Name: 'Allstate Corp',
+      LastPrice: 100 });
+  });
+  after(() => {
+    nock.restore();
+    clock.restore();
+  });
+  describe('constructor', function () {
+    it('should construct a new stock object', function () {
+      const s1 = new Stock('all');
+      expect(s1).to.be.instanceof(Stock);
+      expect(s1.symbol).to.equal('ALL');
+    });
+  });
+  describe('purchase', function () {
+    it('should purchase stocks', function (done) {
+      const s1 = new Stock('all');
+      clock.tick(150);
+      s1.purchase(50, function (err, totalprice) {
+        expect(s1).to.be.instanceof(Stock);
+        expect(s1.shares).to.equal(50);
+        expect(s1.purchasePricePerShare).to.equal(100);
+        expect(s1.purchaseDate.getTime()).to.equal(150);
+        expect(s1.name).to.equal('Allstate Corp');
+        expect(totalprice).to.equal(5000);
+        done();
       });
     });
-    describe('sell', function( ) {
-      it('should return cash value of stock sold', function(done) {
-        const s1 = new Stock('ALL');
-        s1.purchase(50, (err, totalprice) => {
-                expect(s1).to.be.instanceof(Stock);
-                expect(s1.shares).to.equal(50);
-                expect(s1.purchasePricePerShare).to.equal(66.475);
-                expect(s1.name).to.equal('Allstate Corp');
-              //expect(s1.totalprice).to.be.above(3300);
-                done();
-        });
-        s1.sell(30, (err, totalcashVal) => {
-                expect(s1.shares).to.equal(20);
-                expect(s1.purchasePricePerShare).to.equal(66.475);
-                expect(s1.totalprice).to.be.below(3300);
-                done();
-        });
+  });
+  describe('sell', function () {
+    it('should return cash value of stock sold', function (done) {
+      const s1 = new Stock('ALL');
+      s1.shares = 60;
+      s1.sell(30, (err, totalcashVal) => {
+        expect(s1.shares).to.equal(30);
+        expect(s1.purchasePricePerShare).to.equal(100);
+        expect(totalcashVal).to.be.below(3300);
+        done();
       });
+    });
+    it('should return an error when trying to sell too much', function (done) {
+      const s1 = new Stock('ALL');
+      s1.shares = 10;
+      s1.sell(30, (err, totalcashVal) => {
+        expect(s1.shares).to.equal(10);
+        expect(err.message).to.equal('Not enough shares');
+        expect(err).to.be.an('error');
+        expect(totalcashVal).to.be.an('undefined');
+        done();
+      });
+    });
   });
 });
